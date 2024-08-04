86c032ca6564912015fdba7d96f4239acd648b58
iangelmanuel <iangelmanueldm@gmail.com>
2024-08-04 09:23:30 -0500

    chore: Add middleware for authentication and session management

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
+/// <reference path="../.astro/db-types.d.ts" />
+/// <reference path="../.astro/actions.d.ts" />
 /// <reference types="astro/client" />
+
+interface User {
+  email: string
+  name: string
+}
+
+declare namespace App {
+  interface Locals {
+    isLoggedIn: boolean
+    isAdmin: boolean
+    user: User | null
+  }
+}
---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { defineMiddleware } from "astro:middleware"
+import { getSession } from "auth-astro/server"
+const notAuthenticatedRoutes = ["/login", "/register"]
+
+export const onRequest = defineMiddleware(
+  async ({ url, locals, redirect, request }, next) => {
+    const session = await getSession(request)
+    const isLoggedIn = !!session
+    const user = session?.user
+
+    locals.isLoggedIn = isLoggedIn
+    locals.user = null
+    locals.isAdmin = false
+
+    if (user) {
+      // TODO:
+      locals.user = {
+        name: user.name!,
+        email: user.email!
+      }
+      locals.isAdmin = user.role === "admin"
+    }
+
+    // TODO: Eventualmente tenemos que controlar el acceso por roles
+    if (!locals.isAdmin && url.pathname.startsWith("/dashboard")) {
+      return redirect("/")
+    }
+
+    if (isLoggedIn && notAuthenticatedRoutes.includes(url.pathname)) {
+      return redirect("/")
+    }
+
+    return next()
+  }
+)
