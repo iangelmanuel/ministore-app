a54a0d90d70360939c9b1e325a35c56d591862b5
iangelmanuel <iangelmanueldm@gmail.com>
2024-08-08 09:50:35 -0500

    chore: Update dependencies and configuration files for Astro integration with React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
-import db from "@astrojs/db";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import netlify from "@astrojs/netlify"
+import db from "@astrojs/db"
+import auth from "auth-astro"
 
-import auth from "auth-astro";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), auth()],
+  integrations: [tailwind(), db(), auth(), react()],
   output: "server",
   adapter: netlify(),
   experimental: {
     actions: true
   }
-});
+})
