88de4e9d459aa03721911e5df6c430b3d2fb88c5
iangelmanuel <iangelmanueldm@gmail.com>
2024-08-08 09:51:32 -0500

    chore: Add Product and ProductImage tables to the database configuration and seed files

--- db/config.ts
+++ db/config.ts
@@ -18,9 +18,36 @@ const Role = defineTable({
   }
 })
 
+const Product = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    description: column.text(),
+    gender: column.text(),
+    price: column.number(),
+    sizes: column.text(),
+    slug: column.text({ unique: true }),
+    stock: column.number(),
+    tags: column.text(),
+    title: column.text(),
+    type: column.text(),
+
+    user: column.text({ references: () => User.columns.id })
+  }
+})
+
+const ProductImage = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    url: column.text(),
+    productId: column.text({ references: () => Product.columns.id })
+  }
+})
+
 export default defineDb({
   tables: {
     User,
-    Role
+    Role,
+    Product,
+    ProductImage
   }
 })
--- db/seed.ts
+++ db/seed.ts
@@ -1,7 +1,7 @@
-import { db, Role, User } from "astro:db"
+import { db, Role, User, Product, ProductImage } from "astro:db"
 import bcrypt from "bcryptjs"
+import { seedProducts } from "./seed-data"
 
-// https://astro.build/db/seed
 export default async function seed() {
   const roles = [
     { id: "admin", name: "Administrator" },
@@ -29,5 +29,35 @@ export default async function seed() {
   await db.insert(Role).values(roles)
   await db.insert(User).values([johnDoe, janeDoe])
 
+  const queries: any = []
+
+  seedProducts.forEach((p) => {
+    const product = {
+      id: crypto.randomUUID(),
+      description: p.description,
+      gender: p.gender,
+      price: p.price,
+      sizes: p.sizes.join(","),
+      slug: p.slug,
+      stock: p.stock,
+      tags: p.tags.join(","),
+      title: p.title,
+      type: p.type,
+      user: johnDoe.id
+    }
+
+    queries.push(db.insert(Product).values(product))
+
+    p.images.forEach((img) => {
+      const image = {
+        id: crypto.randomUUID(),
+        url: img,
+        productId: product.id
+      }
+
+      queries.push(db.insert(ProductImage).values(image))
+    })
+  })
+  await db.batch(queries)
   console.log("Database seeded successfully")
 }
