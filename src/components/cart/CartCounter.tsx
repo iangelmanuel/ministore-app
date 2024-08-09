da8ddb0190a666f5fc8f4d5c9afe57394c959282
iangelmanuel <iangelmanueldm@gmail.com>
2024-08-09 17:21:41 -0500

    feat: Update CartCounter to dynamically display the number of items in the cart

--- src/components/cart/CartCounter.tsx
+++ src/components/cart/CartCounter.tsx
@@ -1,12 +1,26 @@
+import { itemsInCart } from "@/store/cart.store"
+import { CartCookiesClient } from "@/utils/cart-cookies"
+import { useStore } from "@nanostores/react"
+import { useEffect } from "react"
+
 export const CartCounter = () => {
+  const $itemsInCart = useStore(itemsInCart)
+
+  useEffect(() => {
+    const cart = CartCookiesClient.getCart()
+    itemsInCart.set(cart.length)
+  }, [])
   return (
     <a
       href="/cart"
       className="relative inline-block"
     >
-      <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
-        3
-      </span>
+      {$itemsInCart > 0 && (
+        <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
+          {$itemsInCart}
+        </span>
+      )}
+
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1.5rem"
